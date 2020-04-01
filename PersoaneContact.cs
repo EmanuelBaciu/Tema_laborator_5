bec1795be380a7e45424a21655fae9cb1fba0fdd
EmanuelBaciu <62619090+EmanuelBaciu@users.noreply.github.com>
2020-04-01 18:01:28 +0300

    Add files via upload

---
+++ PersoaneContact.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieTipAgenda
+{
+    
+    public enum Grup
+    {
+        Prieteni=0,
+        Serviciu=1,
+        Familie=2,
+    }
+    /// <summary>
+    /// Persoanele de contact din agenda
+    /// </summary>
+    /// clasa persoanecontact cu parametri auto-implemented
+   public class PersoaneContact
+    {
+        
+        public const int MARE = 1;
+        public const int EGAL = 0;
+        public const int MIC = 1;
+
+        public Grup grup;
+        public Grup Grup { get { return grup; } set { grup = value; } }
+        
+        public string nume { set; get; } 
+        public string prenume { set; get; }
+        public string numarTelefon { set; get; }
+        public string adresaEmail { set; get; }
+
+        public DateTime DataNasterii { set; get; }
+        public string NumeleComplet { get { return nume + "-" + prenume; } }
+
+
+        public PersoaneContact()
+        {
+            nume = string.Empty;
+            prenume = string.Empty;
+            numarTelefon = string.Empty;
+            adresaEmail = string.Empty;
+            
+
+            
+
+        }
+        /// <summary>
+        /// Constructor cu parametri
+        /// </summary>
+        /// <param name="numePersoana"></param>
+        /// <param name="nrTel"></param>
+        /// <param name="e_mail"></param>
+        public PersoaneContact(string numePersoana, string prenumePers, string nrTel, string e_mail, Grup _grup)
+       {
+            nume = numePersoana;
+            prenume = prenumePers;
+          numarTelefon = nrTel;
+            adresaEmail = e_mail;
+            grup =(Grup) _grup;
+            
+
+            
+
+        }
+        /// <summary>
+        /// Constructor care primeste un sir de caractere
+        /// </summary>
+        /// <param name="pers1"></param>
+        public PersoaneContact(string pers1)
+        {
+            string[] buff = pers1.Split();
+            nume = buff[0];
+            prenume = buff[1];
+            numarTelefon = buff[2];
+            adresaEmail = buff[3];
+            grup = (Grup)Convert.ToInt32(buff[4]);
+            Enum.TryParse(buff[5], out grup);
+
+        }
+        
+
+        public static int optGrup()
+        {
+            Console.WriteLine("Alege o optiune: ");
+            Console.WriteLine("0. Prieteni\n"+
+                              "2. Serviciu\n"+
+                              "3. Familie");
+            int opt = Convert.ToInt32(Console.ReadLine());
+            return opt;
+        }
+
+        // Afisare date despre contact
+        public string ConversieLaSir()
+        {
+            string buff = "";
+            buff +=  "Persoana de contact are numele " + nume + "-" + prenume + " cu numarul de telefon: " + numarTelefon + "  adresa de e-mail: " + adresaEmail + " din grupul " + grup;
+            return buff;
+        }
+        public string nastere()
+        {
+            return "Data nasterii: " + DataNasterii;
+            
+        }
+        public int Compara(PersoaneContact _persCon)
+        {
+            return this.NumeleComplet.CompareTo(_persCon.NumeleComplet);
+        }
+        
+        public string ToString()
+        {
+            return nume + " " + prenume + " face parte din grupul: " + Grup;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplicatieTipAgenda
+{
+    class Program
+    {
+        public static PersoaneContact citestePersoana()
+        {
+            string num, pren, numarTel, Email, grup1;
+            Grup grup;
+            Console.WriteLine("Citeste numele persoanei: ");
+            num = Console.ReadLine();
+            Console.WriteLine("Citeste prenumele persoanei: ");
+            pren = Console.ReadLine();
+            Console.WriteLine("Citeste numarul de telefon al persoanei: ");
+            numarTel = Console.ReadLine();
+            Console.WriteLine("Citeste emai-ul persoanei: ");
+            Email = Console.ReadLine();
+            Console.WriteLine("Citeste grupul persoanei: ");
+            grup1 = Console.ReadLine();
+            Enum.TryParse(grup1, out grup);
+
+            return new PersoaneContact(num, pren, numarTel, Email,grup);
+            
+        }
+        
+        
+
+        static void Main(string[] args)
+        {
+            // Initializarea unui obiect de tip persoana cu parametri 
+            PersoaneContact p0 = new PersoaneContact("Baciu","Emanuel","0743307248","emy_baciu@yahoo.com",Grup.Familie);
+            Console.WriteLine(p0.ConversieLaSir());
+            Console.WriteLine(p0.DataNasterii = new DateTime(1999,01,03)) ;
+
+            Console.ReadKey();
+
+            //Intantierea unui obiect de tip Persoana cu parametrii intr-un sir de caractere
+            PersoaneContact p1 = new PersoaneContact("Donici","Andreea","0756818340","andreea@yahoo.com",Grup.Familie);
+            Console.WriteLine(p1.ConversieLaSir());
+            Console.WriteLine(p1.DataNasterii = new DateTime(1999, 07, 31));
+
+            //Instantierea unui obiect de tip Persoana cu parametrii introdusi de la tastatura
+            Console.WriteLine("\nIntroduceti numele, prenumele, numarul de telefon, adresa de e-mail a persoanei de contact! ");
+            PersoaneContact p2 = new PersoaneContact(Console.ReadLine(),Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Grup.Familie);
+
+            Console.WriteLine("\nIntroduceti data nasterii in ordine: anul,luna,ziua:YYYY-XZ-HH");
+            p2.DataNasterii = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine(p2.ConversieLaSir());
+            Console.WriteLine( p2.nastere());
+
+            if (p1.Compara(p2) == PersoaneContact.MARE)
+            {
+                Console.WriteLine(p1.NumeleComplet + " Sunt egale " + p2.NumeleComplet);
+            }
+            else if(p1.Compara(p2) == PersoaneContact.EGAL)
+            {
+                Console.WriteLine(p1.NumeleComplet + " este mai mare decat " + p2.NumeleComplet);
+            }
+            else
+            {
+                Console.WriteLine(p2.NumeleComplet +" este mai mare decat " +p1.NumeleComplet);
+            }
+         
+            PersoaneContact persoanaCititaTas = citestePersoana();
+            Console.WriteLine(persoanaCititaTas);
+
+            Console.ReadKey();
+        }
+    }
+}
